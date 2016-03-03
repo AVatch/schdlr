423b35a56abc59e8cd04a77937d063d0fb70e817
Adrian Vatchinsky <avatchinsky@outlook.com>
2016-03-02 21:42:53 -0500

    issues with db.py

--- schdlr_service/db.py
+++ schdlr_service/db.py
@@ -4,8 +4,7 @@ from sqlalchemy.orm import sessionmaker
 from models import init_db
 
 engine = create_engine('sqlite:///jobs.sqlite', echo=False)
-Session = Session = sessionmaker(bind=engine)
-    
-if __name__ == '__main__':
-    print "yo"
-    init_db(engine)
+Session = sessionmaker(bind=engine)
+
+def get_session(engine):
+    return sessionmaker(bind=engine)
