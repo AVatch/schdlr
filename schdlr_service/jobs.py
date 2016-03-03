cdbca487748ee5391ac7a097b22212d78aef928b
Adrian Vatchinsky <avatchinsky@outlook.com>
2016-03-03 12:52:11 -0500

    updated jobs

--- schdlr_service/jobs.py
+++ schdlr_service/jobs.py
@@ -4,13 +4,15 @@ import json
 from datetime import datetime
 import requests
 
+UPDATE_ENDPOINT = 'https://5379926d.ngrok.com/jobs'
+
 def update_callback(url, data):
     requests.put( url, data=data )
 
 def job_get(**kwargs):
     """ """
     r = requests.get( kwargs.get('url'), headers=kwargs.get('headers', None), params=kwargs.get('params', None) )
-    update_callback( 'https://5379926d.ngrok.com/jobs', 
+    update_callback( UPDATE_ENDPOINT, 
                      data={ 
                             'job_id': kwargs.get('job_id'), 
                             'status_code': r.status_code 
@@ -19,6 +21,11 @@ def job_get(**kwargs):
 def job_post(**kwargs):
     """ """
     r = requests.post( kwargs.get('url'), headers=kwargs.get('headers', None), data=kwargs.get('data', None) )
+    update_callback( UPDATE_ENDPOINT, 
+                     data={ 
+                            'job_id': kwargs.get('job_id'), 
+                            'status_code': r.status_code 
+                          } )
 
 def my_listener(event):
     if event.exception:
