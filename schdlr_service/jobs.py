553392d31b8ae766d29a9e6491b53fda70270482
Adrian Vatchinsky <avatchinsky@outlook.com>
2016-03-02 21:09:24 -0500

    saving the response of the HTTP GET when the job is completed

--- schdlr_service/jobs.py
+++ schdlr_service/jobs.py
@@ -1,19 +1,33 @@
 """
 """
+import json
+from datetime import datetime
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 import requests
 
+from db import Session
 from models import ArchivedJob, init_db
 
-engine = create_engine('sqlite:///jobs.sqlite', echo=False)
-init_db(engine)
-Session = sessionmaker(bind=engine)
+def htt
 
 def job_get(**kwargs):
     """ """
-    requests.get( kwargs.get('url'), headers=kwargs.get('headers', None), params=kwargs.get('params', None) )
+    r = requests.get( kwargs.get('url'), headers=kwargs.get('headers', None), params=kwargs.get('params', None) )
+    
+    # update the archived job object
+    session = Session()
+    job = session.query(ArchivedJob).filter_by( id=kwargs.get('job_id') ).first()
+    job.status = True
+    job.response = json.dumps({
+        'status_code': r.status_code,
+        'content': r.text
+    })
+    job.time_completed = datetime.now()
+    session.commit()
+    
 
 def job_post(**kwargs):
     """ """
